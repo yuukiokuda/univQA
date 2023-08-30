af324d0b0532195cebc0b0c5c58dea348411abae
yuukiokuda <136579520+yuukiokuda@users.noreply.github.com>
2023-08-30 16:17:04 +0900

    css修正

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,7 +2,7 @@ class Genre < ActiveHash::Base
   self.data = [
     { id: 1, name: 'ジャンル選択' },
     { id: 2, name: 'パソコン' },
-    { id: 3, name: '一人暮らし' },
+    { id: 3, name: '1人暮らし' },
     { id: 4, name: '授業' },
     { id: 5, name: 'アルバイト' },
     { id: 6, name: '部活' },
